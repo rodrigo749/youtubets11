cbfb01703389e1472e2c011c8fdab46c496fa811
nucleotechcsl <nucleotechcsl@gmail.com>
2021-01-26 11:37:29 -0300

    create env process

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
 import app from './app';
 import 'reflect-metadata';
 import './database';
+
+
+dotenv.config();
 
 app.listen(3000, () => {
   console.log('🏃 Running Server');
